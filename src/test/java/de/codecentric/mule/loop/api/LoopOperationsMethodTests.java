8912ea5868b420852db578e71bd22571d3437dbc
Roger Butenuth <roger.butenuth@codecentric.de>
2022-10-24 11:38:40 +0200

    Typo in name.

--- src/test/java/de/codecentric/mule/loop/api/LoopOperationsMethodTests.java
+++ src/test/java/de/codecentric/mule/loop/api/LoopOperationsMethodTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class LoopOperatonsMethodTests {
+public class LoopOperationsMethodTests {
 
 	@Test
 	public void conditionNullIsFalse() throws Exception {
